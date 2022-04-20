fdd79635f2f41856ed6ee25eb7d5278657458b7d
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-04-21 03:35:46 +0530

    Add project files.

---
+++ odd and even number whit array/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace odd_and_even_number_whit_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr;
+            int size;
+
+            Console.Write("Enter the size of array  ");
+            size = int.Parse(Console.ReadLine());
+            Console.WriteLine("My array  {0} ",size);
+            arr = new int[size];
+            for (int i = 0; i <size; i++)
+            {
+                Console.Write("number ({0}) :",i+1);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    Console.WriteLine("Even number {0} ", arr[i] + " ");
+                }
+            }
+            
+            Console.WriteLine("===================");
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] % 2 != 0)
+
+                {
+                    Console.WriteLine("odd number {0} ", arr[i] + " ");
+                }
+            }
+        }
+    }
+}
